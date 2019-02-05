9484dd3348848b9581a86b04c24355c1ae9ee578
Osman Rafi <rafi.ogchy@gmail.com>
2019-02-05 23:24:34 +0600

    Fixed mark as best comment

--- resources/views/Discussion/show.blade.php
+++ resources/views/Discussion/show.blade.php
@@ -9,7 +9,8 @@
             <div class="card">
                 <div class="card-header float-left">
                     <img src="{{$post->user->avatar}}" alt="" width="70" height="70">
-                    <b style="font-family: 'Comic Sans MS';font-size: large">{{strtoupper($post->user->name)}} (Points: {{$post->user->points}})</b>
+                    <b style="font-family: 'Comic Sans MS';font-size: large">{{strtoupper($post->user->name)}}
+                        (Points: {{$post->user->points}})</b>
                     <span class="float-right">
                         <a class="badge badge-secondary"
                            href="{{route('channel',['slug'=> $post->channel->slug])}}">{{$post->channel->title}}</a>
@@ -51,7 +52,8 @@
                         <div class="card" style="padding: 40px">
                             <div class="card-header float-left alert alert-success">
                                 <b style="font-family: 'Comic Sans MS';font-size: large"
-                                   class="float-left">{{strtoupper($best_answer->user->name)}} (Points: {{$best_answer->user->points}}) </b>
+                                   class="float-left">{{strtoupper($best_answer->user->name)}}
+                                    (Points: {{$best_answer->user->points}}) </b>
                                 <span class="float-right">
                                 <b>{{$best_answer->created_at->diffForHumans()}}</b>
                             </span>
@@ -91,7 +93,8 @@
                                 <div class="row justify-content-center">
                                     <div class="card">
                                         <div class="card-header float-left">
-                                            <b style="font-family: 'Comic Sans MS';font-size: large">{{strtoupper($r->user->name)}} (Points: {{$r->user->points}})</b>
+                                            <b style="font-family: 'Comic Sans MS';font-size: large">{{strtoupper($r->user->name)}}
+                                                (Points: {{$r->user->points}})</b>
                                             <span class="float-right">
                                                 <b>{{$r->created_at->diffForHumans()}}</b>
                                             </span>
@@ -122,8 +125,11 @@
                                             @endif
 
                                             @if(!$best_answer)
-                                                <a href="{{route('discussion.best.answer',['id' => $r->id])}}"
-                                                   class="btn btn-sm btn-info float-right">Mark as Best answer</a>
+                                                @if(Auth::id() == $post->user->id)
+                                                    <a href="{{route('discussion.best.answer',['id' => $r->id])}}"
+                                                       class="btn btn-sm btn-info float-right">Mark as Best answer</a>
+
+                                                @endif
                                             @endif
                                         </div>
 
